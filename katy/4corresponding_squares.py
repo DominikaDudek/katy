6fac2b79db671362188dc0eac42a9d39bde49589
ddudek <dominika.dudek@nokia.com>
2019-01-21 12:45:20 +0100

    new solution

--- katy/4corresponding_squares.py
+++ katy/4corresponding_squares.py
@@ -3,10 +3,12 @@
 #         square of it in the second one.
 
 def has_corresponding_squares(first_argument, second_argument):
-    for element in first_argument:
-        if not(element**2 in second_argument) or len(first_argument) != len(second_argument):
-            return False
-    return True
+    squared = [element ** 2 for element in first_argument]
+    return sorted(squared) == sorted(second_argument)
+    # for element in first_argument:
+    #     if not(element**2 in second_argument) or len(first_argument) != len(second_argument):
+    #         return False
+    # return True
 
 assert has_corresponding_squares([1, 2, 3], [1, 4, 9]) is True
 assert has_corresponding_squares([1], [1, 5]) is False
